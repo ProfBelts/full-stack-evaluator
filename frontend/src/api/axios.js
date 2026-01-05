4de94a34bbd7d780b28dfdb87b9da4e0f97e137f
ProfBelts <profbelts@gmail.com>
2026-01-06 00:50:09 +0800

    Add api url on axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "http://localhost:5215/api", 
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
 export default api;
